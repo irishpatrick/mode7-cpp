25037064186a4d520764a0c1680a0b860ba64ae8
Patrick Roche <rochep63@gmail.com>
2020-06-01 13:25:00 -0500

    add bullet setup/destroy

---
+++ src/Physics.cpp
@@ -0,0 +1,27 @@
+#include "Physics.hpp"
+#include <btBulletDynamicsCommon.h>
+
+static btBroadphaseInterface* broadphase = nullptr;
+static btDefaultCollisionConfiguration* collisionConfiguration = nullptr;
+static btCollisionDispatcher* dispatcher = nullptr;
+static btSequentialImpulseConstraintSolver* solver = nullptr;
+static btDiscreteDynamicsWorld* world = nullptr;
+
+void Physics::init()
+{
+    broadphase = new btDbvtBroadphase();
+    collisionConfiguration = new btDefaultCollisionConfiguration();
+    dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    solver = new btSequentialImpulseConstraintSolver();
+    world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+    world->setGravity(btVector3(0, -9.81f, 0));
+}
+
+void Physics::destroy()
+{
+    delete world;
+    delete solver;
+    delete dispatcher;
+    delete collisionConfiguration;
+    delete broadphase;
+}
