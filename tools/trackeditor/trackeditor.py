51093c931f5e6d839f6cc3d960b1b58f62fac808
Patrick Roche <rochep63@gmail.com>
2021-04-22 18:13:57 -0400

    work on proper track editor

--- tools/trackeditor/trackeditor.py
+++ tools/trackeditor/trackeditor.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 import tkinter as tk
+from tkinter import filedialog
 import pickle
+import re
 
 class Piece:
     def __init__(self):
+        self.x = 0
+        self.y = 0
         self.name = "Unknown"
         self.type = -1
         self.limits = []
@@ -15,7 +19,15 @@ class Piece:
         pass
 
     def assemble(self, fn):
-        pass
+        fp = open(fn, "r")
+        for line in fp:
+            line = line[:-1]
+            parts = re.split(",| |\t", line)
+            if line.startswith("name"):
+                self.name = parts[1]
+            elif line.startswith("v"):
+                val = [int(i) for i in parts[1:]]
+                self.verts.append((val[0], val[1]))
 
     def draw(self, canvas, origin):
         pass
@@ -29,6 +41,29 @@ class Track:
     def draw(self):
         pass
 
+class TrackCreator:
+    def __init__(self, master):
+        top = self.top = tk.Toplevel(master)
+        top.title("New Track Dialogue")
+        #tk.Grid.rowconfigure(top, 0, weight=1)
+        tk.Grid.columnconfigure(top, 1, weight=1)
+        self.l = tk.Label(top, text="Name")
+        self.l.grid(row=0, column=0, sticky="W")
+        self.e = tk.Entry(top)
+        self.e.grid(row=0, column=2, sticky="EW")
+        self.b = tk.Button(top, text="Ok", command=self.cleanup)
+        self.b.grid(row=1, column=0, columnspan=3, sticky="EW")
+
+        self.value = "Bad Value"
+
+        top.transient(master)
+        top.grab_set()
+        master.wait_window(top)
+
+    def cleanup(self):
+        self.value = self.e.get()
+        self.top.destroy()
+
 class App(tk.Frame):
     def __init__(self):
         super().__init__()
@@ -38,13 +73,52 @@ class App(tk.Frame):
         self.current_track = None
 
     def initui(self):
-        self.master.title("Track Editor")
+        root = self.master
+        root.title("Track Editor")
         self.pack(fill=tk.BOTH, expand=1)
 
         self.canvas = tk.Canvas(self)
         self.canvas.bind('<Button-1>', self.leftclick_cb)
         self.canvas.bind('<Motion>', self.mousemove_cb)
         self.canvas.pack(fill=tk.BOTH, expand=1)
+
+        menubar = tk.Menu(root)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New Track", command=self.new_track)
+        filemenu.add_command(label="Open Track", command=self.open_track)
+        filemenu.add_command(label="Save Track", command=self.save_track)
+        filemenu.add_command(label="Import Piece", command=self.import_piece)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        root.config(menu=menubar)
+
+    def new_track(self):
+        w = TrackCreator(self.master)
+        self.current_track = Track()
+        self.current_track.name = w.value
+
+    def open_track(self):
+        pass
+
+    def save_track(self):
+        fp = open("{}.trk".format(self.current_track.name), "wb")
+        pickle.dump(self.current_track, fp)
+
+    def import_piece(self):
+        fn = filedialog.askopenfilename(
+            initialdir="./", title="Import Piece",
+            filetypes=(
+                ("Track Pieces", "*.tp"),
+                ("All Files", "*")))
+
+        if fn == "":
+            return
+
+        p = Piece()
+        p.assemble(fn)
+        fp = open("pieces/{}.tpb".format(p.name), "wb")
+        pickle.dump(p, fp)
+        
 
     def leftclick_cb(self, event):
         pass
@@ -53,7 +127,7 @@ class App(tk.Frame):
         pass
 
     def mousemove_cb(self, event):
-        print(event)
+        pass#print(event)
 
 def main():
     root = tk.Tk()
